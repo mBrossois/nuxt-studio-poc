d5245951c1aa2e2fd1b0a2bab914c2c0bd7c6a9e
Mark de Neut Brossois <mark.dnb@live.nl>
2025-03-19 14:11:18 +0100

    setup preview api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@nuxt/ui'],
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  },
   css: ['~/assets/css/main.css']
 })
