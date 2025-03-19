d13f7d0385452fa3261b410ae7db6cc413655934
Mark de Neut Brossois <mark.dnb@live.nl>
2025-03-19 14:17:35 +0100

    Added input component

---
+++ components/atoms/input.vue
@@ -0,0 +1,19 @@
+<template>
+    <input class="input" :class="{'bg-color': hasBackgroundColor}" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    hasBackgroundColor: boolean
+}>()
+</script>
+
+<style scoped>
+.input {
+    background-color: none;
+}
+
+.bg-color {
+    background-color: white;
+}
+</style>
