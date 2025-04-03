b3806e3c799f35d2c2954bac4a1bdeabf0ba4942
Mark de Neut Brossois <mark.dnb@live.nl>
2025-04-03 15:24:47 +0200

    hmmm

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,5 @@
 export default defineAppConfig({
-    global: {
+    Global: {
         en: {
           NO: 'NO',
           YES: 'YES'
@@ -9,7 +9,7 @@ export default defineAppConfig({
           YES: 'OUI'
         }
     },
-    secondPage: {
+    SecondPage: {
       en: {
         HELLO: 'Hello'
       },
