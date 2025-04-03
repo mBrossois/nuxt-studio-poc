ac11cdf1523ccaceca02a3f1ffc967178fd7dbd2
Mark de Neut Brossois <mark.dnb@live.nl>
2025-04-03 10:54:34 +0200

    Updated content.config

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,20 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
     content: defineCollection({
       source: '**',
-      type: 'page'
+      type: 'page',
+      schema: z.object({
+        translations: z.object({
+          en: z.object({
+            hello: z.string()
+          }),
+          fr: z.object({
+            hello: z.string()
+          })
+        })
+      })
     })
   },
 })
