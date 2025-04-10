4459a05239dc2ff76259b75087a4081d0d1d84cd
Mark de Neut Brossois <mark.dnb@live.nl>
2025-04-10 14:41:56 +0200

    Added field for fr translation

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -4,7 +4,6 @@ export default defineNuxtSchema({
   appConfig: {
     Global: {
       en: group({
-        type: 'object',
         title: 'en',
         description: 'Translations',
         icon: 'i-ph-palette-fill',
@@ -21,6 +20,14 @@ export default defineNuxtSchema({
         title: 'fr',
         description: 'Translations',
         icon: 'i-ph-palette-fill',
+        fields: {
+          NO: field({
+            type: 'string',
+            title: 'NO',
+            description: 'No translation',
+            icon: 'i-ph-palette-fill',
+          })
+        }
       })
     },
     SecondPage: {
