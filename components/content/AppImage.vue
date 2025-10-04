2a5fc5e9f246dcd02ec18ec7b1f77dbac19a211c
Mark de Neut Brossois <markdeneutbrossois@Marks-MacBook-Air.local>
2025-10-04 11:31:28 +0200

    Added image component

---
+++ components/content/AppImage.vue
@@ -0,0 +1,12 @@
+<template>
+    <img :src="src" :alt="alt" :width="width" :height="height" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    src: string
+    alt: string
+    width: string
+    height: string
+}>()
+</script>
